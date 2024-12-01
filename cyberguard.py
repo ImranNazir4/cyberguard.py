a2655f7321c3b5d09ddbfa19854efef581ab248f
Imran Nazir <106988509+ImranNazir4@users.noreply.github.com>
2024-12-02 00:22:08 +0500

    Update cyberguard.py

--- cyberguard.py
+++ cyberguard.py
@@ -77,8 +77,18 @@ rag_chain = (
 )
 
 
-
-
+#######################################################################
+    colorama.init()
+    def type(words: str):
+        for char in words:
+            sleep(0.015)
+            sys.stdout.write(char)
+            sys.stdout.flush()
+        # print()
+    
+    url = r'https://www.virustotal.com/vtapi/v2/file/scan'
+    api= os.getenv("VT_API_KEY")
+#######################################################################
 
 st.subheader("Malicious File Scanner")
 
@@ -89,41 +99,24 @@ if selection=="Cyber Awareness Chatbot":
     query=st.text_input("Write Query Here")
     if st.button("Submit"):
         st.write(rag_chain.invoke(query).content)
-
-file=st.file_uploader("Select a File")
-
-
-colorama.init()
-def type(words: str):
-    for char in words:
-        sleep(0.015)
-        sys.stdout.write(char)
-        sys.stdout.flush()
-    print()
-
-url = r'https://www.virustotal.com/vtapi/v2/file/scan'
-
-# api = open("vt-api.txt", "r").read()
-api= os.getenv("VT_API_KEY")
-
-# file_path = colorama.Fore.YELLOW + "cui2020.pdf"
-# params = {"apikey": api}
-
-with open(file.name, mode='wb') as w:
-        w.write(file.getvalue())
-
-file_to_upload = {"file": open(file.name, "rb")}
-
-response = requests.post(url,files = file_to_upload , params=params)
-file_url = f"https://www.virustotal.com/api/v3/files/{(response.json())['sha1']}"
-
-headers = {"accept": "application/json", "x-apikey": api}
-type(colorama.Fore.YELLOW + "Analysing....")
-
-response = requests.get(file_url,headers=headers)
-
-report = response.text
-report = json.loads(report)
-# json_string = json.dumps(report)
-
-st.write(response)
+if selection=="Malicious File Scanner":
+    file=st.file_uploader("Select a File")
+    if file!=None st.button("Analyze"):     
+        with open(file.name, mode='wb') as w:
+                w.write(file.getvalue())
+        
+        file_to_upload = {"file": open(file.name, "rb")}
+        
+        response = requests.post(url,files = file_to_upload , params=params)
+        file_url = f"https://www.virustotal.com/api/v3/files/{(response.json())['sha1']}"
+        
+        headers = {"accept": "application/json", "x-apikey": api}
+        type(colorama.Fore.YELLOW + "Analysing....")
+        
+        response = requests.get(file_url,headers=headers)
+        
+        report = response.text
+        report = json.loads(report)
+        # json_string = json.dumps(report)
+        
+        st.write(response)
